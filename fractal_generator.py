9f5b304452fb6ada8afa85b7e007128d509abcc8
milckywayy <dmitriwear@gmail.com>
2023-08-08 23:18:54 +0200

    Switch to OpenCV

--- fractal_generator.py
+++ fractal_generator.py
@@ -1,37 +1,45 @@
-from PIL import Image, ImageDraw
+import cv2
+import numpy as np
 import math
 
 
 class FractalGenerator:
     def __init__(self):
-        self.width, self.height = 1500, 1500
-        self.image = Image.new("RGB", (self.width, self.height), "black")
-        self.draw = ImageDraw.Draw(self.image)
+        self.width, self.height = 2000, 2000
+        self.image = np.zeros((self.height, self.width, 3), dtype=np.uint8)
 
         self.tree_color = (255, 255, 255)
+        self.background_color = (30, 30, 30)
 
+        self.tree_thickness = 3
         self.angle_step = 45
 
     def generate_tree(self):
-        start_point = (self.width / 2, self.height * 2/3)
+        self.image[np.all(self.image <= 100, axis=-1)] = self.background_color
 
-        self.draw.line([(self.width / 2, self.height), start_point], fill=self.tree_color, width=2)
+        start_point = (int(self.width / 2), int(self.height * 2/3))
 
+        # Draw root
+        cv2.line(self.image, (int(self.width / 2), int(self.height)), start_point, self.tree_color, self.tree_thickness, cv2.LINE_AA)
         self.recursive_draw(9, start_point, self.height / 9, 90)
-        self.image.save("test.png")
+
+        cv2.imshow('Fractal Tree Generator', self.image)
+
+    def write_to_file(self, file_path):
+        cv2.imwrite(file_path, self.image)
 
     def recursive_draw(self, iterations, pos, length, angle):
         if iterations < 1:
             return
 
         for i in range(2):
-            # find next point
+            # Find next point
             phi = (angle + (self.angle_step / 2)) - (self.angle_step * i)
-            x = pos[0] - (math.cos(math.radians(phi)) * length)
-            y = pos[1] - (math.sin(math.radians(phi)) * length)
+            x = int(pos[0] - (math.cos(math.radians(phi)) * length))
+            y = int(pos[1] - (math.sin(math.radians(phi)) * length))
 
-            # draw line
-            self.draw.line([pos, (x, y)], fill=self.tree_color, width=2)
+            # Draw line
+            cv2.line(self.image, pos, (x, y), self.tree_color, self.tree_thickness, cv2.LINE_AA)
 
-            # go deeper
+            # Go deeper
             self.recursive_draw(iterations - 1, (x, y), length * 0.8, phi)
--- main.py
+++ main.py
@@ -4,5 +4,11 @@ from fractal_generator import *
 if __name__ == '__main__':
     gen = FractalGenerator()
 
+    cv2.namedWindow('Fractal Tree Generator', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Fractal Tree Generator', 500, 500)
+
     gen.generate_tree()
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
